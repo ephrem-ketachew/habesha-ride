85dabfc10b15442d025fdd1529460d383752e07b
yordi2323 <yhailu.cctechet@gmail.com>
2025-11-04 11:15:03 +0300

    feat(user): implement user profile and update

---
+++ src/validation/user.schema.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+import { sanitizeInput } from "../utils/sanitize.util.js";
+
+const capitalize = (val: string) =>
+  val.trim().toLowerCase().replace(/^\w/, (c) => c.toUpperCase());
+
+export const updateMeSchema = z
+  .object({
+    firstName: z
+      .string()
+      .trim()
+      .min(2, "First name must be at least 2 characters")
+      .max(50, "First name cannot exceed 50 characters")
+      .transform(sanitizeInput)
+      .transform(capitalize)
+      .optional(),
+
+    lastName: z
+      .string()
+      .trim()
+      .min(2, "Last name must be at least 2 characters")
+      .max(50, "Last name cannot exceed 50 characters")
+      .transform(sanitizeInput)
+      .transform(capitalize)
+      .optional(),
+
+    phoneNumber: z
+      .string()
+      .trim()
+      .regex(/^[0-9]{7,15}$/, "Phone number must contain 7-15 digits")
+      .optional(),
+
+  //  profileImage: z.string().url().optional(),
+  })
+  .strict();
+
+export type UpdateProfileInput = z.infer<typeof updateMeSchema>;
